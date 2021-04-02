77fceef81333f2a360d4941fb0c994cf3212b163
Rene Gonzalez <rene_glez1992@hotmail.com>
2021-04-01 18:42:55 -0700

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Actividad_Integradora_4_Problema_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblContador = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.txtAño = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnInteractuar = new System.Windows.Forms.Button();
+            this.txtMesCumpleaños = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.rtbCumpleñeros = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Contador";
+            // 
+            // lblContador
+            // 
+            this.lblContador.AutoSize = true;
+            this.lblContador.Location = new System.Drawing.Point(130, 53);
+            this.lblContador.Name = "lblContador";
+            this.lblContador.Size = new System.Drawing.Size(13, 13);
+            this.lblContador.TabIndex = 1;
+            this.lblContador.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Apellido:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Fecha de Nacimiento";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Dia:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(118, 166);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Mes:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(190, 166);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Año:";
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(74, 163);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(37, 20);
+            this.txtDia.TabIndex = 3;
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(144, 163);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(37, 20);
+            this.txtMes.TabIndex = 4;
+            // 
+            // txtAño
+            // 
+            this.txtAño.Location = new System.Drawing.Point(216, 163);
+            this.txtAño.Name = "txtAño";
+            this.txtAño.Size = new System.Drawing.Size(37, 20);
+            this.txtAño.TabIndex = 5;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(104, 83);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(104, 109);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(116, 192);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnInteractuar
+            // 
+            this.btnInteractuar.Location = new System.Drawing.Point(104, 292);
+            this.btnInteractuar.Name = "btnInteractuar";
+            this.btnInteractuar.Size = new System.Drawing.Size(75, 23);
+            this.btnInteractuar.TabIndex = 8;
+            this.btnInteractuar.Text = "Interactuar";
+            this.btnInteractuar.UseVisualStyleBackColor = true;
+            this.btnInteractuar.Click += new System.EventHandler(this.btnInteractuar_Click);
+            // 
+            // txtMesCumpleaños
+            // 
+            this.txtMesCumpleaños.Location = new System.Drawing.Point(156, 266);
+            this.txtMesCumpleaños.Name = "txtMesCumpleaños";
+            this.txtMesCumpleaños.Size = new System.Drawing.Size(66, 20);
+            this.txtMesCumpleaños.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(51, 269);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(99, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Busqueda por Mes:";
+            // 
+            // rtbCumpleñeros
+            // 
+            this.rtbCumpleñeros.Location = new System.Drawing.Point(54, 321);
+            this.rtbCumpleñeros.Name = "rtbCumpleñeros";
+            this.rtbCumpleñeros.Size = new System.Drawing.Size(168, 96);
+            this.rtbCumpleñeros.TabIndex = 17;
+            this.rtbCumpleñeros.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 438);
+            this.Controls.Add(this.rtbCumpleñeros);
+            this.Controls.Add(this.txtMesCumpleaños);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnInteractuar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtAño);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.txtDia);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblContador);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Actividad Integradora 4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblContador;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.TextBox txtAño;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnInteractuar;
+        private System.Windows.Forms.TextBox txtMesCumpleaños;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RichTextBox rtbCumpleñeros;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Actividad_Integradora_4_Problema_3
+{
+    public struct Persona
+    {
+        public string nombre;
+        public string apellido;
+        public string año;
+        public string mes;
+        public string dia;
+
+        public Persona(string sNombre, string sApellido, string sAño, string sMes, string sDia)
+        {
+            nombre = sNombre;
+            apellido = sApellido;
+            año = sAño;
+            mes = sMes;
+            dia = sDia;
+        }
+    }
+    public partial class Form1 : Form
+    {
+        Persona[] persona = new Persona[8];
+        int contador = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            lblContador.Text = contador.ToString();
+            txtNombre.Focus();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (contador < 8)
+            {
+                if (!string.IsNullOrEmpty(txtNombre.Text))
+                {
+                    persona[contador] = new Persona();
+                    persona[contador].nombre = txtNombre.Text;
+                    persona[contador].apellido = txtApellido.Text;
+                    persona[contador].año = txtAño.Text;
+                    persona[contador].mes = txtMes.Text;
+                    persona[contador].dia = txtDia.Text;
+                    contador++;
+                    lblContador.Text = contador.ToString();
+                    Limpiar();
+                }
+                else
+                {
+                    MessageBox.Show("Debe capturar antes los datos","Alerta");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ya no puede registrar a mas usuarios", "Alerta");
+            }
+        }
+        private void Limpiar()
+        {
+            txtNombre.Text = string.Empty;
+            txtNombre.Focus();
+            txtApellido.Text = string.Empty;
+            txtAño.Text = string.Empty;
+            txtMes.Text = string.Empty;
+            txtDia.Text = string.Empty;
+        }
+
+        private void btnInteractuar_Click(object sender, EventArgs e)
+        {
+            string sDato = string.Empty;
+            for (int i= 0; i<8; i++)
+            {
+                if (txtMesCumpleaños.Text == "0")
+                {
+                    MessageBox.Show("Fin de Interaccion");
+                    return;
+                }
+                else
+                { 
+                    if (Convert.ToInt32(persona[i].mes) == Convert.ToInt32(txtMesCumpleaños.Text))
+                    {
+                        sDato = string.Format("{0}\r\n{1}", sDato,persona[i].nombre);
+                    }
+                }
+            }
+            rtbCumpleñeros.Text = sDato;
+        }
+    }
+}
